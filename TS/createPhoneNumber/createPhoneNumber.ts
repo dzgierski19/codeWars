7f9c5d7d516b57d82a6b1b5d8fe08c8fbad05a81
dzgierski19 <dzgierskigit@gmail.com>
2023-09-30 10:26:49 +0200

    createPhoneNumber

---
+++ TS/createPhoneNumber/createPhoneNumber.ts
@@ -0,0 +1,32 @@
+export function createPhoneNumber(numbers: number[]): string {
+  if (numbers.length !== 10) {
+    throw new Error("Pleease type array with 10 numbers");
+  }
+  const stringed = numbers.map((element) => String(element));
+  return stringed.reduce((acc, element, index) => {
+    if (index >= 0 && index < 2) {
+      acc += element;
+    }
+    if (index === 2) {
+      acc += element + ") ";
+    }
+    if (index >= 3 && index < 5) {
+      acc += element;
+    }
+    if (index === 5) {
+      acc += element + "-";
+    }
+    if (index >= 6) {
+      acc += element;
+    }
+    return acc;
+  }, "(");
+}
+
+// (123) 456-7890"
+
+const exampleNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const result = createPhoneNumber(exampleNumber);
+
+console.log(result);
